9c3d5de64743db851352bff22badcaa1aa536c19
aniket <dummyani26@gmail.com>
2022-02-15 22:17:42 +0530

    Handle invalid or unauthorized class code

--- src/components/Classroom.jsx
+++ src/components/Classroom.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Announcement from "./Announcement";
 
 function Classroom() {
     const { code } = useParams()
     const [announcements, setAnnouncements] = useState([])
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         const fetchClassroom = async () => {
@@ -17,11 +18,23 @@ function Classroom() {
             }
 
             const response = await fetch(`http://localhost:8000/api/classes/${code}`, options)
+            if (!response.ok) {
+                setError(true)
+                return
+            }
             const data = await response.json()
             setAnnouncements(data.announcements)
         }
         fetchClassroom()
     }, [code])
+
+    if (error)
+        return (
+            <div>
+                <h1>Invalid class code</h1>
+                <Link to='/'>Back to Classes</Link>
+            </div>
+        )
 
     return (
         <div>
