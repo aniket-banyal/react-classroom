1429e88b1c77bc31f34a8bebc784337fc3839588
aniket <dummyani26@gmail.com>
2022-02-16 14:48:17 +0530

    Display announcement author name

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -4,6 +4,7 @@ function Announcement({ announcement }) {
 
     return (
         <Box sx={{ border: 1, borderColor: 'black', marginTop: 5 }}>
+            <p> {announcement.author_name} </p>
             <pre> {announcement.text} </pre>
         </Box >
     )
