2e6c4d1d563c05727015fcbf52fca9e36b86c471
aniket <dummyani26@gmail.com>
2022-03-02 23:33:34 +0530

    Make CommentSection collapsable

--- src/components/Comment/CommentSection.jsx
+++ src/components/Comment/CommentSection.jsx
@@ -1,14 +1,16 @@
-import { LinearProgress, Typography } from "@mui/material"
+import { Button, LinearProgress, Typography } from "@mui/material"
 import { useParams } from "react-router-dom"
 import useComments from "../../hooks/api/useComments"
 import Comment from "./Comment"
 import CreateComment from "./CreateComment"
 import { Stack } from "@mui/material"
 import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import { useState } from "react"
 
 function CommentSection({ announcementId }) {
     const { code } = useParams()
     const { data: comments, isLoading } = useComments(code, announcementId)
+    const [collapsed, setCollapsed] = useState(true)
 
 
     return (
@@ -17,28 +19,37 @@ function CommentSection({ announcementId }) {
                 :
                 <>
                     {comments.length > 0 &&
-                        <Stack
-                            direction='row'
-                            spacing={1}
-                            sx={{ pt: 1, pl: 2 }}
+                        <Button
+                            size='small'
+                            onClick={() => setCollapsed(!collapsed)}
+                            focusRipple={false}
+                            sx={{ mt: 1, ml: 1, px: 1 }}
                         >
-                            <PeopleOutlineIcon />
+                            <Stack
+                                direction='row'
+                                spacing={1}
+                            >
+                                <PeopleOutlineIcon />
 
-                            <Typography variant="subtitle1">
-                                Comments
-                            </Typography>
+                                <Typography variant="subtitle1">
+                                    Comments ({comments.length})
+                                </Typography>
+                            </Stack>
+                        </Button>
+                    }
+
+                    {!collapsed &&
+                        <Stack spacing={1}>
+                            {comments.map(comment =>
+                                <Comment
+                                    key={comment.id}
+                                    comment={comment}
+                                    announcementId={announcementId}
+                                />
+                            )}
                         </Stack>
                     }
 
-                    <Stack spacing={1}>
-                        {comments.map(comment =>
-                            <Comment
-                                key={comment.id}
-                                comment={comment}
-                                announcementId={announcementId}
-                            />
-                        )}
-                    </Stack>
                     <CreateComment announcementId={announcementId} />
                 </>
             }
