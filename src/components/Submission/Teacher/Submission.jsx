ecb004d44a8f6377a9e859bcc0714b0de31c6303
aniket <dummyani26@gmail.com>
2022-03-08 22:11:37 +0530

    Use DataGrid to show Submissions
    Display all submissions and individual submission together

--- src/components/Submission/Teacher/Submission.jsx
+++ src/components/Submission/Teacher/Submission.jsx
@@ -1,16 +1,19 @@
-import { Box, Button } from "@mui/material"
+import { Box, Button, Card, CardContent, Stack, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { addErrorToast } from "../../../helpers/addToast"
+import { useAssignmentPoints } from "../../../hooks/api/useAssignment"
 import useGradeSubmission from "../../../hooks/api/useGradeSubmission"
 import useCreateDateTime from "../../../hooks/useCreateDateTime"
+import UserAvatar from '../../UserAvatar';
 
 
-function Submission({ submission, totalPoints }) {
+function Submission({ submission }) {
     const [showGradingInp, setShowGradingInp] = useState(false)
     const [points, setPoints] = useState('')
-    const submittedAt = useCreateDateTime(submission.submission?.created_at)
     const { code, assignment_id } = useParams()
+    const submittedAt = useCreateDateTime(submission.submission?.created_at)
+    const { data: totalPoints } = useAssignmentPoints(code, assignment_id)
     const { mutate } = useGradeSubmission()
 
     const handleSubmit = async e => {
@@ -39,44 +42,60 @@ function Submission({ submission, totalPoints }) {
 
 
     return (
-        <Box sx={{ borderBottom: 1, borderColor: 'black', padding: 1 }}>
-            <div style={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'space-between' }}>
-                <p> {submission.student.name} </p>
-                <div>
-                    <p> {submission.status} </p>
-                    <p> {submittedAt} </p>
-                </div>
-            </div>
-            {submission.submission &&
-                <>
-                    <Button
-                        variant="contained"
-                        target="_blank"
-                        href={submission.submission.url}
+        <Card>
+            <CardContent>
+                <Stack spacing={2}>
+                    <Stack direction='row' alignItems='center' spacing={2}>
+                        <UserAvatar name={submission.fullName} />
+                        <Typography variant='h6'>
+                            {submission.fullName}
+                        </Typography>
+                    </Stack>
+
+                    <Box>
+                        <Typography variant='subtitle1'>
+                            {submission.status}
+                        </Typography>
+
+                        <Typography variant='subtitle2'>
+                            {submittedAt}
+                        </Typography>
+                    </Box>
+                </Stack>
+
+                {submission.submission &&
+                    <Box
+                        sx={{ mt: 2 }}
                     >
-                        Submission
-                    </Button>
+                        <Button
+                            variant="contained"
+                            target="_blank"
+                            href={submission.submission.url}
+                        >
+                            Submission
+                        </Button>
 
-                    {showGradingInp &&
-                        <form onSubmit={handleSubmit} >
-                            <label> {`Grade (out of ${totalPoints})`} : </label>
-                            <input
-                                type="number"
-                                placeholder='Points'
-                                min={0}
-                                max={totalPoints}
-                                value={points}
-                                onChange={e => setPoints(e.target.value)}
-                            />
+                        {showGradingInp &&
+                            <form onSubmit={handleSubmit}>
+                                <label> {`Grade (out of ${totalPoints})`} : </label>
+                                <input
+                                    type="number"
+                                    placeholder='Points'
+                                    min={0}
+                                    max={totalPoints}
+                                    value={points}
+                                    onChange={e => setPoints(e.target.value)}
+                                />
 
-                            <input type="submit" value='Grade' />
-                        </form>
-                    }
+                                <input type="submit" value='Grade' />
+                            </form>
+                        }
 
-                    {submission.submission.points && <p> Graded: {`${submission.submission.points}/${totalPoints}`} points</p>}
-                </>
-            }
-        </Box>
+                        {submission.submission.points && <p> Graded: {`${submission.submission.points}/${totalPoints}`} points</p>}
+                    </Box>
+                }
+            </CardContent>
+        </Card >
     )
 }
 
--- src/components/Submission/Teacher/Submissions.jsx
+++ src/components/Submission/Teacher/Submissions.jsx
@@ -1,14 +1,93 @@
-import { useState } from "react"
+import { Button, Grid, Typography } from "@mui/material"
+import { DataGrid } from "@mui/x-data-grid"
+import { useEffect, useMemo, useState } from "react"
 import { useParams } from "react-router-dom"
-import { useAssignmentPoints } from "../../../hooks/api/useAssignment"
+import useGradeSubmission from "../../../hooks/api/useGradeSubmission"
 import useSubmissions from "../../../hooks/api/useSubmissions"
+import useUserRole from "../../../hooks/api/useUserRole"
 import Submission from "./Submission"
 
 
 function Submissions() {
+    const [rows, setRows] = useState([])
+    const [selectedSubmission, setSelectedSubmission] = useState(null)
     const { code, assignment_id } = useParams()
     const { data: submissions, isLoading } = useSubmissions(code, assignment_id)
-    const { data: totalPoints } = useAssignmentPoints(code, assignment_id)
+    const { data: userRole } = useUserRole(code)
+    const { mutate } = useGradeSubmission()
+
+    const columns = useMemo(() => {
+        let cols = [
+            {
+                field: 'fullName',
+                headerName: 'Full name',
+                width: 160,
+            },
+            {
+                field: 'email',
+                headerName: 'Email',
+                type: 'string',
+                width: 300,
+            },
+            {
+                field: 'status',
+                headerName: 'Status',
+                type: 'string',
+                width: 150,
+            },
+            {
+                field: 'grade',
+                headerName: 'Grade',
+                type: 'string',
+                width: 100,
+                renderCell: ({ row }) => {
+                    if (row.status === 'Graded')
+                        return row.grade
+                    return '-'
+                }
+            },
+            {
+                field: 'submission',
+                headerName: 'Submission',
+                type: 'string',
+                width: 150,
+                renderCell: ({ row }) => {
+                    if (row.status === 'Done' || row.status === 'Submitted Late' || row.status === 'Graded') {
+                        return (
+                            <Button
+                                onClick={() => setSelectedSubmission(row)}
+                            >
+                                Submission
+                            </Button>
+                        )
+                    }
+                    return '-'
+                }
+            },
+        ]
+
+        return cols
+    }, [mutate, userRole, code])
+
+    useEffect(() => {
+        if (!submissions)
+            return
+
+        let rows = []
+        submissions.forEach(submission => {
+            let newRow = {
+                id: submission.student.email,
+                fullName: submission.student.name,
+                email: submission.student.email,
+                status: submission.status,
+                grade: submission.submission?.points,
+                submission: submission.submission
+            }
+
+            rows.push(newRow)
+        })
+        setRows(rows)
+    }, [submissions])
 
 
     if (isLoading) {
@@ -18,16 +97,23 @@ function Submissions() {
     }
 
     return (
-        <div>
-            <h1> Submissions </h1>
-            {submissions.map(submission =>
-                <Submission
-                    key={submission.student.email}
-                    submission={submission}
-                    totalPoints={totalPoints}
+        <Grid container spacing={5}>
+            <Grid item xs={8}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    disableSelectionOnClick
+                    disableColumnMenu
+                    autoHeight
                 />
-            )}
-        </div>
+            </Grid>
+
+            <Grid item xs={4}>
+                {selectedSubmission &&
+                    <Submission submission={selectedSubmission} />
+                }
+            </Grid >
+        </Grid >
     )
 }
 
