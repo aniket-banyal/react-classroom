aabb29bd2965310e6cbb121133cb94d87d316d48
aniket <dummyani26@gmail.com>
2022-03-11 15:01:07 +0530

    Improve UI of LoginPage

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, Grid, Stack, Typography } from "@mui/material"
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import useAuth from "../hooks/useAuth"
@@ -13,14 +14,32 @@ function LoginPage() {
     }, [isAuth])
 
     return (
-        <div>
-            {!isAuth &&
-                <>
-                    <h2>Login to classroom</h2>
-                    <Login />
-                </>
-            }
-        </div>
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{ minHeight: '100vh' }}
+        >
+            <Grid item>
+                <Card
+                    sx={{ minHeight: 200 }}
+                >
+                    <CardContent>
+                        {!isAuth &&
+                            <Stack spacing={12} alignItems='space-between'>
+                                <Typography variant='h5'>
+                                    Login to Classroom
+                                </Typography>
+
+                                <Login />
+                            </Stack>
+                        }
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
     )
 }
 
