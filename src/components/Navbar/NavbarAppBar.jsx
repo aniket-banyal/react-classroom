6f8d800ec564d84328e90b8fa298cf7f254aed4f
aniket <dummyani26@gmail.com>
2022-03-11 12:40:49 +0530

    Separate out NavbarMenu

--- src/components/Navbar/NavbarAppBar.jsx
+++ src/components/Navbar/NavbarAppBar.jsx
@@ -1,53 +1,13 @@
-import { AppBar, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import AddIcon from '@mui/icons-material/Add';
-import { useState } from 'react';
-import JoinClassroom from '../JoinClassroom';
-import CreateClassroom from '../CreateClassroom';
-import BasicModal from "../BasicModal"
 import UserIconMenu from './UserIconMenu';
+import NavbarMenu from './NavbarMenu';
 
 
 function NavbarAppBar({ toggleDrawer, drawerOpen }) {
-    const [anchorEl, setAnchorEl] = useState(null)
-    const [joining, setJoining] = useState(false)
-    const [creating, setCreating] = useState(false)
-
-    const handleClose = () => setAnchorEl(null)
-
-    const handleJoinClass = () => {
-        setJoining(true)
-        handleClose()
-    }
-
-    const handleCreateClass = () => {
-        setCreating(true)
-        handleClose()
-    }
-
-
-    const handleMenu = event => setAnchorEl(event.currentTarget)
-
 
     return (
         <>
-            <BasicModal
-                open={joining}
-                setOpen={setJoining}
-                title='Join Classroom'
-            >
-                <JoinClassroom onSubmit={() => setJoining(false)} />
-            </BasicModal>
-
-            <BasicModal
-                open={creating}
-                setOpen={setCreating}
-                title='Create Classroom'
-            >
-                <CreateClassroom onSubmit={() => setCreating(false)} />
-            </BasicModal>
-
-
             <AppBar position="fixed">
                 <Toolbar>
                     <IconButton
@@ -58,30 +18,13 @@ function NavbarAppBar({ toggleDrawer, drawerOpen }) {
                     >
                         <MenuIcon />
                     </IconButton>
+
                     <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
                         Classroom
                     </Typography>
 
-                    <IconButton
-                        disableFocusRipple
-                        size="large"
-                        onClick={handleMenu}
-                    >
-                        <AddIcon />
-                    </IconButton>
-
-                    <Menu
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                    >
-                        <MenuItem onClick={handleJoinClass}>Join Class</MenuItem>
-                        <MenuItem onClick={handleCreateClass}>Create Class</MenuItem>
-                    </Menu>
-
+                    <NavbarMenu />
                     <UserIconMenu />
-
                 </Toolbar>
             </AppBar>
             <Toolbar />
---
+++ src/components/Navbar/NavbarMenu.jsx
@@ -0,0 +1,68 @@
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+import { IconButton, Menu, MenuItem } from '@mui/material';
+import JoinClassroom from '../JoinClassroom';
+import CreateClassroom from '../CreateClassroom';
+import BasicModal from "../BasicModal";
+
+
+function NavbarMenu() {
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [joining, setJoining] = useState(false)
+    const [creating, setCreating] = useState(false)
+
+    const handleClose = () => setAnchorEl(null)
+    const handleMenu = event => setAnchorEl(event.currentTarget)
+
+    const handleJoinClass = () => {
+        setJoining(true)
+        handleClose()
+    }
+
+    const handleCreateClass = () => {
+        setCreating(true)
+        handleClose()
+    }
+
+
+    return (
+        <>
+            <BasicModal
+                open={joining}
+                setOpen={setJoining}
+                title='Join Classroom'
+            >
+                <JoinClassroom onSubmit={() => setJoining(false)} />
+            </BasicModal>
+
+            <BasicModal
+                open={creating}
+                setOpen={setCreating}
+                title='Create Classroom'
+            >
+                <CreateClassroom onSubmit={() => setCreating(false)} />
+            </BasicModal>
+
+
+            <IconButton
+                disableFocusRipple
+                size="large"
+                onClick={handleMenu}
+            >
+                <AddIcon />
+            </IconButton>
+
+            <Menu
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleJoinClass}>Join Class</MenuItem>
+                <MenuItem onClick={handleCreateClass}>Create Class</MenuItem>
+            </Menu>
+        </>
+    )
+}
+
+export default NavbarMenu
