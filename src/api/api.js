eae211782328a84418abe02be9e6d0c4eed19200
aniket <dummyani26@gmail.com>
2022-03-13 17:53:45 +0530

    Add axios interceptor to refresh token

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL = 'http://localhost:8000/api'
 
-export const api = axios.create({
+const api = axios.create({
     baseURL: baseURL,
     headers: {
         Authorization: localStorage.getItem('access_token') ?
@@ -10,3 +10,35 @@ export const api = axios.create({
         'Content-Type': 'application/json',
     }
 })
+
+api.interceptors.response.use(
+    response => response,
+    async (error) => {
+        if (error.response.status === 401) {
+            const body = {
+                refresh_token: localStorage.getItem('refresh_token'),
+                grant_type: 'refresh_token',
+                client_id: 'sz8z14oUhS1fd0yEqWp38vzoM3f9VrJQ2Fe5JEkI',
+                client_secret: '5ZYLS56MgsSftQq4Tn3ISamn9sQ8mBvybfRslK0HCYLTXteCJF9MpXl3RILdrWeXlRAq8tRHtv34hi1xaRkHrtey28FThhZOezSBAGeNRQKEBrp7NRnSoaI0OeWNfX1m'
+            }
+            const options = {
+                method: 'POST',
+                headers: new Headers({
+                    'content-Type': 'application/json',
+                }),
+                body: JSON.stringify(body)
+            }
+
+            const response = await fetch('http://localhost:8000/auth/token', options)
+            const data = await response.json()
+
+            localStorage.setItem('access_token', data.access_token)
+            localStorage.setItem('refresh_token', data.refresh_token)
+            localStorage.setItem('is_auth', true)
+
+            api.defaults.headers['Authorization'] = `Bearer ${data.access_token}`
+        }
+    }
+)
+
+export { api }
