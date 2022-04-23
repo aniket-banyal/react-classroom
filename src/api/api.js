a73f1a5e26fc0c1383967d74ae827b6e4ef159fd
aniket <dummyani26@gmail.com>
2022-04-23 10:23:28 +0530

    Fix- Refresh token when access token expires

--- src/api/api.js
+++ src/api/api.js
@@ -10,33 +10,67 @@ const api = axios.create({
     }
 })
 
+let authTokenRequest;
+
+// This function makes a call to get the auth token
+// or it returns the same promise as an in-progress call to get the auth token
+function getAuthToken() {
+    if (!authTokenRequest) {
+        authTokenRequest = makeAuthenticationRequest();
+        authTokenRequest.then(resetAuthTokenRequest, resetAuthTokenRequest);
+    }
+    return authTokenRequest;
+}
+
+function resetAuthTokenRequest() {
+    authTokenRequest = null;
+}
+
+function saveTokens(data) {
+    localStorage.setItem('access_token', data.access_token)
+    localStorage.setItem('refresh_token', data.refresh_token)
+    localStorage.setItem('is_auth', true)
+
+    api.defaults.headers['Authorization'] = `Bearer ${data.access_token}`
+}
+
+async function makeAuthenticationRequest() {
+    const body = {
+        refresh_token: localStorage.getItem('refresh_token'),
+        grant_type: 'refresh_token',
+        client_id: process.env.REACT_APP_DJANGO_CLIENT_ID,
+        client_secret: process.env.REACT_APP_DJANGO_CLIENT_SECRET
+    }
+    const options = {
+        method: 'POST',
+        headers: new Headers({
+            'content-Type': 'application/json',
+        }),
+        body: JSON.stringify(body)
+    }
+
+    const response = await fetch(`${process.env.REACT_APP_DJANGO_AUTH_URL}/token`, options)
+    if (response.status === 400)
+        return null
+
+    const data = await response.json()
+    return data
+}
+
 api.interceptors.response.use(
     response => response,
-    async (error) => {
-        if (error.response.status === 401) {
-            const body = {
-                refresh_token: localStorage.getItem('refresh_token'),
-                grant_type: 'refresh_token',
-                client_id: process.env.REACT_APP_DJANGO_CLIENT_ID,
-                client_secret: process.env.REACT_APP_DJANGO_CLIENT_SECRET
+    async (err) => {
+        const error = err.response;
+
+        if (error.status === 401 && error.config && !error.config.__isRetryRequest) {
+            const data = await getAuthToken()
+            if (data) {
+                saveTokens(data);
+                error.config.__isRetryRequest = true;
+                return axios(error.config);
             }
-            const options = {
-                method: 'POST',
-                headers: new Headers({
-                    'content-Type': 'application/json',
-                }),
-                body: JSON.stringify(body)
-            }
-
-            const response = await fetch(`${process.env.REACT_APP_DJANGO_AUTH_URL}/token`, options)
-            const data = await response.json()
-
-            localStorage.setItem('access_token', data.access_token)
-            localStorage.setItem('refresh_token', data.refresh_token)
-            localStorage.setItem('is_auth', true)
-
-            api.defaults.headers['Authorization'] = `Bearer ${data.access_token}`
         }
+
         else {
             throw error
         }
