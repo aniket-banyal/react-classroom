6561a41fea4ffb1ae8afff43c87f0de186bc072a
aniket <dummyani26@gmail.com>
2022-04-25 21:50:22 +0530

    Change layout of Assignment

--- src/components/Assignment/Assignment.jsx
+++ src/components/Assignment/Assignment.jsx
@@ -53,32 +53,32 @@ function Assignment({ assignment }) {
             <Card>
                 <CardActionArea component={Link} to={`/${code}/assignments/${assignment.id}`}>
                     <CardContent>
-                        <Stack direction='row' alignItems='center' justifyContent='space-between'>
-                            <Box sx={{ width: '60%' }}>
-                                <Typography
-                                    variant="h5"
-                                    color='primary'
-                                    noWrap
-                                    gutterBottom
-                                >
-                                    {assignment.title}
-                                </Typography>
-
-                                <Typography variant="subtitle1">
-                                    {assignment.points} points
-                                </Typography>
-                            </Box>
-
-                            <Stack direction='row' alignItems='center' spacing={2}>
-                                <Typography
-                                    variant="subtitle1"
-                                    color='text.secondary'
-                                >
-                                    Due {dueDateTime}
-                                </Typography>
+                        <Stack>
+                            <Stack direction='row' alignItems='center' justifyContent='space-between'>
+                                <Box sx={{ width: '60%' }}>
+                                    <Typography
+                                        variant="h5"
+                                        color='primary'
+                                        noWrap
+                                        gutterBottom
+                                    >
+                                        {assignment.title}
+                                    </Typography>
+                                </Box>
 
                                 {userRole === 'teacher' && <ThreeDotMenu options={options} />}
                             </Stack>
+
+                            <Typography variant="subtitle1">
+                                {assignment.points} points
+                            </Typography>
+
+                            <Typography
+                                variant="subtitle2"
+                                color='text.secondary'
+                            >
+                                Due {dueDateTime}
+                            </Typography>
                         </Stack>
                     </CardContent>
                 </CardActionArea>
