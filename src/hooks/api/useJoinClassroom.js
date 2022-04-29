2c671eeb5ebb8c665ee11c1f1b86f1f216e90723
aniket <dummyani26@gmail.com>
2022-04-29 19:44:37 +0530

    Change join classroom url

--- src/hooks/api/useJoinClassroom.js
+++ src/hooks/api/useJoinClassroom.js
@@ -3,7 +3,7 @@ import { api } from "../../api";
 
 
 const joinClassroom = async (code) => {
-    const { data } = await api.post(`/classes/${code}/students`)
+    const { data } = await api.post(`/classes/${code}/students/`)
     return data
 }
 
